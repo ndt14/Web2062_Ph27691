1bbcf1e15be6351feb57f8d585b1a24dbc4792d5
unknown <thinh1420003@gmail.com>
2023-01-09 18:00:50 +0700

    107 section9

---
+++ Section9/107_ShortCircuiting/script.js
@@ -0,0 +1,54 @@
+const restaurant ={
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23,Firenze, Italy',
+    categories: ['Italian','Pizzeria','Vegetarian','Organic'],
+    starterMenu: ['Focaccia','Bruschetta','Garlic Bread','Caprese Salad'],
+    mainMenu: ['Pizza','Pasta','Risotto'],
+    openingHours: {
+        thu:{
+            open:12,
+            close:22,
+        },
+        fri:{
+            open:11,
+            close:23,
+        },
+        sat:{
+            open:0,
+            close:24,
+        },
+    },
+    order: function(starterIndex, mainIndex){
+        return[this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery: function(starterIndex, mainIndex, time, address) {
+        console.log('Order received! ${this.starterMenu[starterIndex]} and {this.mainMenu[mainIndex]} will be delivery to ${address} at ${time}');
+    },
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    },
+    orderPizza:function(mainIngredient,...otherIngredients){
+console.log(mainIngredient);
+console.log(otherIngredients);
+
+    }
+};
+//or
+console.log(3 || 'jonas');
+console.log(''|| 'jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0|| ''||'Hello'|| 23|| null);
+restaurant.numGuests =23;
+const guests1 =restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+//and
+console.log(0 && 'jonas');
+console.log(7 && 'jonas');
+console.log('hello' && 23 && null &&'jonas');
+if(restaurant.orederPizza){
+    restaurant.orderPizza('mushrooms','spinach');
+}
+restaurant.orderPizza&&  restaurant.orderPizza('mushrooms','spinach');
