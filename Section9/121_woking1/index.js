c3920efc070c1cb7c663ece3880e91436ad26694
unknown <thinh1420003@gmail.com>
2023-01-15 22:24:04 +0700

    121 setion9

---
+++ Section9/121_woking1/index.js
@@ -0,0 +1,74 @@
+const weekeday  =['mon','tue','wed','thu','fri','sat','sun'];
+const openingHours= {
+    [weekeday[3]]:{
+        open:12,
+        close:22,
+    },
+    [weekeday[4]]:{
+        open:11,
+        close:23,
+    },
+    [weekeday[5]]:{
+        open:0,
+        close:12+12,
+    },
+};
+const restaurant ={
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23,Firenze, Italy',
+    categories: ['Italian','Pizzeria','Vegetarian','Organic'],
+    starterMenu: ['Focaccia','Bruschetta','Garlic Bread','Caprese Salad'],
+    mainMenu: ['Pizza','Pasta','Risotto'],
+    openingHours,
+   
+    order(starterIndex, mainIndex){
+        return[this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery(starterIndex, mainIndex, time, address) {
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivery to ${address} at ${time}`);
+    },
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    },
+    orderPizza(mainIngredient,...otherIngredients){
+console.log(mainIngredient);
+console.log(otherIngredients);
+
+    }
+};
+const airline = 'TAP AIR PORTUGAL';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4,7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.indexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function(seat){
+    // B and C are middle seats
+    const s = seat.slice(-1);
+    if(s === 'B' || s === 'E')
+    console.log('you got the middle seat');
+    else console.log('you got lucky');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('Jonas'));
