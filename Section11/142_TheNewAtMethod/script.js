9d53700cbfc2ed15578540e05547ebe50862e949
unknown <thinh1420003@gmail.com>
2023-01-31 22:39:16 +0700

    142

---
+++ Section11/142_TheNewAtMethod/script.js
@@ -0,0 +1,10 @@
+const arr =[23,11,64];
+console.log(arr);
+console.log(arr.at(0));
+// getting last array element
+console.log(arr[arr.length-1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('thinh'.at(0));
+console.log('thinh'.at(-1));
